637fc64d96ea9c252d48e953e64a5bb132bf7c6f
Adinai Iuldasheva <80417831+s20218@users.noreply.github.com>
2022-11-09 16:21:25 +0100

    Add files via upload

---
+++ tutorial-6-ihord-s20218/LinqTutorial/LinqTutorials/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+            var t2 = LinqTasks.Task2();
+            var t3 = LinqTasks.Task3();
+            var t4 = LinqTasks.Task4();
+            var t5 = LinqTasks.Task5();
+            var t6 = LinqTasks.Task6();
+            var t7 = LinqTasks.Task7();
+            var t8 = LinqTasks.Task8();
+            var t9 = LinqTasks.Task9();
+            var t10 = LinqTasks.Task10();
+            var t11 = LinqTasks.Task11();
+            var t12 = LinqTasks.Task12();
+            int[] arr = { 1, 1, 2, 2, 3, 3, 4 };
+            var t13 = LinqTasks.Task13(arr);
+            var t14 = LinqTasks.Task14();
+
+        }
+    }
+}
